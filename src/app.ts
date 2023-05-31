cad86b4533cac925be55cc08a67a70f783ffb205
jonathanosouza <jonathanosouza@gmail.com>
2023-05-31 09:01:58 -0300

    Alterando versão node

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import fastify from 'fastify';
 import { env } from './env';
 import cookie from '@fastify/cookie';
 import { BooksSearchRoutes } from './routes/BooksSearch';
-import fastifyCors from 'fastify-cors';
+// import fastifyCors from 'fastify-cors';
 
 export const app = fastify();
 
@@ -10,6 +10,6 @@ export const app = fastify();
 app.register(BooksSearchRoutes, { prefix: 'livros' });
 
 // Registre o plugin fastify-cors
-app.register(fastifyCors, {
-  origin: true, // Habilita todas as origens (pode ajustar conforme necessário)
-});
+// app.register(fastifyCors, {
+//   origin: true, // Habilita todas as origens (pode ajustar conforme necessário)
+// });
