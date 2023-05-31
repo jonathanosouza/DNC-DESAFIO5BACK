9892eadfcb9dfbe26e7750a104faae5a2d76644c
jonathanosouza <jonathanosouza@gmail.com>
2023-05-31 12:57:55 -0300

    Alterando db para postgres

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import fastify from 'fastify';
 import { env } from './env';
 import cookie from '@fastify/cookie';
 import { BooksSearchRoutes } from './routes/BooksSearch';
-// import fastifyCors from 'fastify-cors';
+import fastifyCors from 'fastify-cors';
 
 export const app = fastify();
 
 // app.register(cookie);
 app.register(BooksSearchRoutes, { prefix: 'livros' });
 
-// Registre o plugin fastify-cors
-// app.register(fastifyCors, {
-//   origin: true, // Habilita todas as origens (pode ajustar conforme necessário)
-// });
+//Registre o plugin fastify-cors
+app.register(fastifyCors, {
+  origin: true, // Habilita todas as origens (pode ajustar conforme necessário)
+});
